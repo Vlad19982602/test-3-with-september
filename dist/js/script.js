3468ef390bb4427df4c0b3b49b52e375b1195e10
Vladislav Fedorov <ma3za@list.ru>
2023-09-17 17:01:53 +0500

    starter pack

---
+++ dist/js/script.js
@@ -0,0 +1,27 @@
+import scrolling from './modules/pageup.js';
+
+
+window.addEventListener('DOMContentLoaded', () => {
+
+	scrolling('.pageup');
+	const menu = document.querySelector('.header-lists'),
+	menuItem = document.querySelectorAll('.header_item'),
+	hamburger = document.querySelector('.hamburger');
+
+	if(hamburger) {
+		hamburger.addEventListener('click', () => {
+			hamburger.classList.toggle('hamburger__active');
+			menu.classList.toggle('header-active');
+		});
+	}
+
+	if(menuItem) {
+		menuItem.forEach(item => {
+			item.addEventListener('click', () => {
+				hamburger.classList.toggle('hamburger__active');
+				menu.classList.toggle('header-active');
+			})
+		})
+	}
+});
+
